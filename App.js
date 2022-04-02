22df7fc833822734b85566e64d97e0ecbf870854
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-04-02 17:03:11 +0100

    implement initial navigator structure

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
-// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-	StyleSheet,
-	View,
-} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // import Homepage from './components/home.jsx';
 // import Icon from 'react-native-vector-icons';
 
 import Login from './screens/login.js';
 import SignUp from './screens/signup.js';
-import Home from './screens/home.js';
+import HomeScreen from './screens/home.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<View>
-			{/* <Login /> */}
-			{/* <SignUp /> */}
-			<Home />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				{/* <Stack.Screen name="Login" component={Login} /> */}
+				{/* <Stack.Screen name="SignUp" component={Signup} /> */}
+				<Stack.Screen
+					name="Home"
+					component={HomeScreen}
+					options={{
+						title: "Sport Share",
+						headerStyle: {
+							backgroundColor: "#183059",
+						},
+						headerTitleStyle: {
+							color: 'white',
+							fontWeight: 'bold',
+						},
+					}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
-const styles = StyleSheet.create({
-});
