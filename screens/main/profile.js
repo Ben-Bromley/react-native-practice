8be35c72f24ef492402c96c4dad77ce515a37200
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-04-04 22:17:58 +0100

    add basic profile content

--- screens/main/profile.js
+++ screens/main/profile.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import {
-	StyleSheet,
-	Text,
-	View,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
 
+import globalStyles from '../../styles/styles';
+
 export default function EventCard() {
-	return (
-		<View style={styles.container}>
-            <Text style={styles.text}>Hello User</Text>
-		</View>
-	);
+
+    let user = {
+        name: 'John Smith',
+    };
+
+    return (
+        <View style={globalStyles.screenContainer}>
+            {/* <View style={globalStyles.hr}></View> */}
+            <View style={globalStyles.profile}>
+                <View>
+                    <Text style={globalStyles.title}>{user.name}</Text>
+                    <Text style={globalStyles.textSecondaryLink}>Edit Your Profile</Text>
+                </View>
+                <View style={globalStyles.profileImg}></View>
+            </View>
+            <View style={globalStyles.optionsList}>
+                <Text style={globalStyles.option}>My Events</Text>
+                <Text style={globalStyles.option}>Friends</Text>
+                <Text style={globalStyles.option}>Account</Text>
+                <Text style={globalStyles.option}>Settings</Text>
+            </View>
+            {/* <View style={globalStyles.hr}></View> */}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     // ------- Profile Styles -------- //
-	container: {
-        backgroundColor: "#1a212e",
-        minHeight: '100%',
-    },
-    text: {
-        color: 'white',
-        fontSize: 15,
-    }
+
 });
