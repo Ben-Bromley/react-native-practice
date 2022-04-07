4c3b979c7afa23a107af7af96ba602aefeb0cafe
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-04-07 13:53:18 +0100

    add rest of pages

--- screens/main/index.js
+++ screens/main/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './home';
-// import SearchScreen from './search';
+import SearchScreen from '../events/search';
+import CreateEvent from '../events/create';
+import SavedEvents from '../events/saved';
 import ProfileScreen from './profile';
+
+
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Root() {
@@ -14,23 +18,25 @@ export default function Root() {
             initialRouteName="Home"
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
-                    switch (route.name) {
-                        case 'Home':
-                            return <Icon name='home' size={size} color={color} />;
-                            break;
-                        case "Profile":
-                            return <Icon name='person' size={size} color={color} />;
-                            break;
+                    let icons = {
+                        Home: 'home',
+                        Search: 'search',
+                        Create: 'add-circle',
+                        Saved: 'bookmark',
+                        Profile: 'person'
                     }
+                    return <Icon name={`${icons[route.name]}${focused ? "" : "-outline"}`} size={size} color={color} />
                 },
                 tabBarStyle: { backgroundColor: '#183059', borderTopWidth: 0 },
                 tabBarActiveTintColor: 'white',
-                tabBarInactiveTintColor: 'gray',
+                tabBarInactiveTintColor: 'lightgray',
                 tabBarShowLabel: false,
                 headerShown: false
             })}>
             <Tab.Screen name="Home" component={HomeScreen} />
-            {/* <Tab.Screen name="Search" component={searchScreen} /> */}
+            <Tab.Screen name="Search" component={SearchScreen} />
+            <Tab.Screen name="Create" component={CreateEvent} />
+            <Tab.Screen name="Saved" component={SavedEvents} />
             <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
     )
