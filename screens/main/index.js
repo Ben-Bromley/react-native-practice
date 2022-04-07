2a0911c1777642648e67b39ce60ee3672889b59b
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-04-07 19:19:25 +0100

    refactor icons, fix param persistence issue

--- screens/main/index.js
+++ screens/main/index.js
@@ -1,43 +1,61 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeScreen from './home';
-import SearchScreen from '../events/search';
-import CreateEvent from '../events/create';
-import SavedEvents from '../events/saved';
-import ProfileScreen from './profile';
+import HomeScreen from "./home";
+import SearchScreen from "../events/search";
+import CreateEvent from "../events/create";
+import SavedEvents from "../events/saved";
+import ProfileScreen from "./profile";
 
-
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default function Root() {
-    const Tab = createBottomTabNavigator();
-    
-    return (
-        <Tab.Navigator
-            initialRouteName="Home"
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let icons = {
-                        Home: 'home',
-                        Search: 'search',
-                        Create: 'add-circle',
-                        Saved: 'bookmark',
-                        Profile: 'person'
-                    }
-                    return <Icon name={`${icons[route.name]}${focused ? "" : "-outline"}`} size={size} color={color} />
-                },
-                tabBarStyle: { backgroundColor: '#183059', borderTopWidth: 0 },
-                tabBarActiveTintColor: 'white',
-                tabBarInactiveTintColor: 'lightgray',
-                tabBarShowLabel: false,
-                headerShown: false
-            })}>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Search" component={SearchScreen} />
-            <Tab.Screen name="Create" component={CreateEvent} />
-            <Tab.Screen name="Saved" component={SavedEvents} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-    )
+	const Tab = createBottomTabNavigator();
+
+	return (
+		<Tab.Navigator
+			initialRouteName="Home"
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ focused, color, size }) => {
+					let icons = {
+						Home: "home",
+						Search: "search",
+						Create: "add-circle",
+						Saved: "bookmark",
+						Profile: "person",
+					};
+					return (
+						<Icon
+							name={`${icons[route.name]}${
+								focused ? "" : "-outline"
+							}`}
+							size={size}
+							color={color}
+						/>
+					);
+				},
+				tabBarStyle: { backgroundColor: "#183059", borderTopWidth: 0 },
+				tabBarActiveTintColor: "white",
+				tabBarInactiveTintColor: "lightgray",
+				tabBarShowLabel: false,
+				headerShown: false,
+			})}
+		>
+			<Tab.Screen
+				name="Home"
+				component={HomeScreen}
+				listeners={({ navigation, route }) => ({
+					tabPress: (e) => {
+                        if (route.params){
+                            route.params.eventCreated = false;
+                        }
+					},
+				})}
+			/>
+			<Tab.Screen name="Search" component={SearchScreen} />
+			<Tab.Screen name="Create" component={CreateEvent} />
+			<Tab.Screen name="Saved" component={SavedEvents} />
+			<Tab.Screen name="Profile" component={ProfileScreen} />
+		</Tab.Navigator>
+	);
 }
