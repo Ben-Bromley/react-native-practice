6658451e164de8db9887d18dd169368b9693b67f
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-04-08 15:35:21 +0100

    basic styles for saved events screen

--- screens/events/saved.js
+++ screens/events/saved.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import globalStyles from '../../styles/styles'
 
 export default function SavedEvents() {
   return (
-    <View>
-      <Text>SavedEvents</Text>
+    <View style={globalStyles.screenContainer}>
+      <Text style={globalStyles.title}>Your Saved Events</Text>
     </View>
   )
 }
