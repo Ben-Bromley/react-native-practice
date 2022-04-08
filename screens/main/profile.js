c5c57482a07f73c9020aae02874702291703e347
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-04-08 15:11:56 +0100

    implement basic logout

--- screens/main/profile.js
+++ screens/main/profile.js
@@ -1,40 +1,48 @@
-import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-import globalStyles from '../../styles/styles';
+import globalStyles from "../../styles/styles";
 
-export default function EventCard() {
+export default function Profile({ navigation }) {
+	let user = {
+		name: "John Smith",
+	};
 
-    let user = {
-        name: 'John Smith',
-    };
+    const logout = () => {
+        // TODO: logout user
+        // navigate to login screen
+        navigation.replace("Login");
+    }
 
-    return (
-        <View style={globalStyles.screenContainer}>
-            {/* <View style={globalStyles.hr}></View> */}
-            <View style={globalStyles.profile}>
-                <View>
-                    <Text style={globalStyles.title}>{user.name}</Text>
-                    <Text style={globalStyles.textSecondaryLink}>Edit Your Profile</Text>
-                </View>
-                <View style={globalStyles.profileImg}></View>
-            </View>
-            <View style={globalStyles.optionsList}>
-                <Text style={globalStyles.option}>My Events</Text>
-                <Text style={globalStyles.option}>Friends</Text>
-                <Text style={globalStyles.option}>Account</Text>
-                <Text style={globalStyles.option}>Settings</Text>
-            </View>
-            {/* <View style={globalStyles.hr}></View> */}
-        </View>
-    );
+	return (
+		<View style={globalStyles.screenContainer}>
+			{/* <View style={globalStyles.hr}></View> */}
+			<View style={globalStyles.profile}>
+				<View>
+					<Text style={globalStyles.title}>{user.name}</Text>
+					<Text style={globalStyles.textSecondaryLink}>
+						Edit Your Profile
+					</Text>
+				</View>
+				<View style={globalStyles.profileImg}></View>
+			</View>
+			<View style={globalStyles.optionsList}>
+				<Text style={globalStyles.option}>My Events</Text>
+				<Text style={globalStyles.option}>Friends</Text>
+				<Text style={globalStyles.option}>Account</Text>
+				<Text style={globalStyles.option}>Settings</Text>
+			</View>
+			{/* <View style={globalStyles.hr}></View> */}
+			<TouchableOpacity
+				style={globalStyles.secondaryButton}
+				onPress={() => {logout()}}
+			>
+				<Text style={globalStyles.textLight}>Logout</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    // ------- Profile Styles -------- //
-
+	// ------- Profile Styles -------- //
 });
